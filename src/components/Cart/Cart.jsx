b3bd11292f3cdc557e7fb1b4cd4734a9fa3b6cfa
Franco Avanzatti <avanzatti5@gmail.com>
2025-05-12 12:51:54 -0300

    ajustes

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 import "./cart.css"
 
 const Cart = () => {
-  const { cart,deleteProduct,deleteCart, totalPrice } = useContext(CartContext)
+  const { cart, deleteProduct, deleteCart, totalPrice } = useContext(CartContext)
 
 
-  if(cart.lenght === 0){
+  if (cart.lenght === 0) {
     return (
       <div>
         <h2>El carrito esta vacío</h2>
@@ -18,33 +18,33 @@ const Cart = () => {
   }
 
   return (
-   <>
-   <div className="container-cart">
-   <div>
-      <ul>
-        {
-          cart.map((productCart) => (
-            <div className="item-cart" key={productCart.id}>
-            <img src={productCart.image} width={100} />
-            <p>{productCart.name}</p>
-            <p>cantidad: {productCart.quantity}</p>
-            <p>precio c/u: {productCart.price.toLocaleString("es-ES")}</p>
-            <button onClick={() => deleteProduct(productCart.id)}>Eliminar</button>
-          </div>
-        ))
-        }
-      </ul>
+    <>
+      <div className="container-cart">
+        <div>
+          <ul>
+            {
+              cart.map((productCart) => (
+                <div className="item-cart" key={productCart.id}>
+                  <img src={productCart.image} width={100} />
+                  <p>{productCart.name}</p>
+                  <p>cantidad: {productCart.quantity}</p>
+                  <p>precio c/u: {productCart.price.toLocaleString("es-ES")}</p>
+                  <button onClick={() => deleteProduct(productCart.id)}>Eliminar</button>
+                </div>
+              ))
+            }
+          </ul>
 
-      <h3>Precio total: {totalPrice().toLocaleString("es-ES")}</h3>
-      <Link className="button-to-cart" to="/checkout">Continuar con mi compra</Link>
-    </div>
-    <button onClick={deleteCart}>Vaciar carrito</button>
+          <h3>Precio total: {totalPrice().toLocaleString("es-ES")}</h3>
+          <Link className="button-to-cart" to="/checkout">Continuar con mi compra</Link>
+        </div>
+        <button onClick={deleteCart}>Vaciar carrito</button>
 
-   </div>
-   
-   </>
+      </div>
 
-    
+    </>
+
+
   )
 }
 export default Cart
