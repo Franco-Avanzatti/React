1222c4824dc678de59b24b5513f31cc59edca871
Franco Avanzatti <avanzatti5@gmail.com>
2025-05-12 15:14:41 -0300

    Ajustes en itemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -5,35 +5,35 @@ const ItemCount = ({ stock, addProductInCart }) => {
   const [count, setCount] = useState(1)
 
   const handleClickRemove = () => {
-    if( count > 1 ){
-      setCount( count - 1 )
+    if (count > 1) {
+      setCount(count - 1)
     }
   }
 
   const handleClickAdd = () => {
-    if( count < stock ){
-      setCount( count + 1 )
+    if (count < stock) {
+      setCount(count + 1)
     }
   }
 
   return (
-   <>
-   <div className="container">
-   <div className="button-detail">
-      <button className="item-count" onClick={handleClickRemove} >-</button>
-      <p>{count}</p>
-      <button className="item-count" onClick={handleClickAdd} >+</button>
-     </div>
+    <>
+      <div className="container">
+        <div className="button-detail">
+          <button className="item-count" onClick={handleClickRemove} >-</button>
+          <p>{count}</p>
+          <button className="item-count" onClick={handleClickAdd} >+</button>
+        </div>
 
-     <div className="add-product">
-      <button className="item-count" onClick={ () => addProductInCart(count) } >Agregar Producto</button>
-     </div>
+        <div className="add-product">
+          <button className="item-count" onClick={() => addProductInCart(count)} >Agregar Producto</button>
+        </div>
 
-   </div>
-    
-    </> 
-    
-    
+      </div>
+
+    </>
+
+
   )
 }
 export default ItemCount
